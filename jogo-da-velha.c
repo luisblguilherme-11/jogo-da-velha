e622a1e7513d32a7c28527b9134084d53d65289a
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-06-17 20:01:23 -0300

    Create jogo-da-velha.c

---
+++ jogo-da-velha.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "jogo-da-velha.h"
+
+int main(int argc, char *argv[]) {
+    executar_jogo(argc, argv);
+    
+    printf("Criado por Luís Guilherme\n");
+    return 0;
+}
